c638213791e6113d830c23ebdf11e84a8a182543
santhi <235429598+tanguturisanthi@users.noreply.github.com>
2026-02-28 00:22:18 +0530

    Add files via upload
    Day 1 - variables, f strings, age calculator

---
+++ day1.py
@@ -0,0 +1,43 @@
+#self introduction
+name="priya"
+age="19" 
+city="nellore"
+course="python full course"
+print(f"{name} is {age} old gurll,living in {city} and learning {course}")
+
+#simple shop bill
+milk_name="milk packet"
+oil_name="freedom oil"
+flour_name="wheat flour"
+milk_price=30
+oil_price=135
+flour_price=60
+print(f'the price of {milk_name} is {milk_price}')
+print(f'the price of {oil_name} is {oil_price}')
+print(f'the price of {flour_name} is {flour_price}')
+print(f'the total amount is: {milk_price+oil_price+flour_price}')
+
+#Favourite things
+movie="shutter island"
+food="jalebi"
+colour="black"
+print(f'my favourite movie is {movie}\n My alltime favourite  food is {food}\n my fvrte colour is {colour}')
+
+
+#swap two values
+a=10
+b=20
+print(a,b)
+a,b=b,a
+print(a,b)
+
+#age calculatorr
+born=int(input("enter your birth year"))
+month=int(input("enter your birth month"))
+age =2026-born
+if month<=2:
+    print(f"current age is {age}")
+else:
+    print(f"current age is {age -1}")    
+
+
