3b6a34e7ab112e4c450a16e4c0dadcc26744395d
tanguturisanthi <235429598+tanguturisanthi@users.noreply.github.com>
2026-08-03 16:14:17 +0530

    day13-list logic practice

--- python-reps/rough.py
+++ python-reps/rough.py
@@ -1,9 +1,28 @@
-sum=0
-while True:
-    n=int(input("enter a num"))
-    if n<0:
+# #   skip mul of 4
+# for i in range(1,21):
+#     if i%4==0:
+#         continue
+#     print(i)
+# variation
+count=0
+while count<3:
+    ans=input("enter an answer yes/no")
+    count+=1
+    if ans== 'yes':
+        print("glad we 're on samme page")
         break
-    if n==0:
-        continue
-    sum+=n
-print(f'{sum}')    
+    elif count>=3:
+        print("we're out of attempts")
+#control the attempts while-else 
+count=0
+while count<3:
+    ans=input('enter yes/no')
+    if ans=='yes':
+        print("glad,we're on same page")
+        break
+    count+=1
+else:
+    print("out of tries")    
+
+
+    
